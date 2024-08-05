07410a64aaced4ecd2d577e73005a49103d7a21c
Zozi <zozi.fer96@gmail.com>
2024-08-05 14:29:14 -0600

    chore: Update setup.py with long description and content type

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='linq-tool',
     version='0.1.0',
     description='A LINQ-like library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Zozimo Fernandez',
     author_email='zozi.fer96@gmail.com',
     packages=find_packages(),
