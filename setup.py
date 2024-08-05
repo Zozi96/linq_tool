ef8498116a32da88bc62495595469832c66b0f66
Zozi <zozi.fer96@gmail.com>
2024-08-05 17:46:19 -0600

    chore: Update setuptools-scm to version 8.1.0 in requirements.txt

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='linq-tool',
-    version='0.1.4',
+    use_scm_version=True,
+    setup_requires=['setuptools-scm'],
     description='A LINQ-like library for Python',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Zozimo Fernandez',
     author_email='zozi.fer96@gmail.com',
+    url='https://github.com/tu_usuario/linq-tool',
     packages=find_packages(),
-    url='https://github.com/Zozi96/linq_tool',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
